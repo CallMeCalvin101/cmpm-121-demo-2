24daeed06aa766da28d4cee541aad7849340668c
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-10-15 01:57:16 -0700

    Step 2 Completed

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,44 @@ const header: HTMLElement | null = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const canvas: HTMLElement | null = document.createElement("canvas");
+// Canvas, clear, and mouse code from https://shoddy-paint.glitch.me/paint0.html
+const canvas = document.createElement("canvas");
 canvas.setAttribute("id", "canvas");
-canvas.setAttribute("height", canvasSize.toString());
-canvas.setAttribute("width", canvasSize.toString());
+canvas.width = canvasSize;
+canvas.height = canvasSize;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx?.beginPath();
+    ctx?.moveTo(cursor.x, cursor.y);
+    ctx?.lineTo(e.offsetX, e.offsetY);
+    ctx?.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+app.append(document.createElement("br"));
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
