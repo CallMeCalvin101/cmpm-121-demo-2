2d9438cb2f815e0fd2cc39e3bb24ec344fbe708e
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-10-15 00:04:51 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "D's Rawing App";
+const canvasSize = 256;
 
 document.title = gameName;
 
-const header = document.createElement("h1");
+const header: HTMLElement | null = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas: HTMLElement | null = document.createElement("canvas");
+canvas.setAttribute("id", "canvas");
+canvas.setAttribute("height", canvasSize.toString());
+canvas.setAttribute("width", canvasSize.toString());
+app.append(canvas);
